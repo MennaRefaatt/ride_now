6ca455c8ac00fb8eccce1f9bc88af1c6ba2513df
MennaRefaatt <bmennarefaat84@gmail.com>
2024-12-04 14:19:06 +0200

    on_boarding

---
+++ lib/core/components/app_name.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ride_now/core/theming/app_colors.dart';
+
+class AppName extends StatelessWidget {
+  const AppName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+            text: 'Ride',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.sp,
+            ),
+            children: const [
+          TextSpan(
+            text: 'N',
+            style: TextStyle(color: AppColors.primary),
+          ),
+          TextSpan(
+            text: 'ow',
+            style: TextStyle(color: Colors.white),
+          ),
+        ]));
+  }
+}
