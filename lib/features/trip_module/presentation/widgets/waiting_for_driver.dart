5ecdf1be8df634fb0f13961115e8a880a77dc454
MennaRefaatt <bmennarefaat84@gmail.com>
2025-01-30 18:29:24 +0200

    some updates

--- lib/features/trip_module/presentation/widgets/waiting_for_driver.dart
+++ lib/features/trip_module/presentation/widgets/waiting_for_driver.dart
@@ -18,7 +18,7 @@ class WaitingForDriver extends StatefulWidget {
 class _WaitingForDriverState extends State<WaitingForDriver>
     with TickerProviderStateMixin {
   final DraggableScrollableController _controller =
-      DraggableScrollableController();
+  DraggableScrollableController();
   double _childSize = 0.4;
 
   @override
@@ -29,112 +29,91 @@ class _WaitingForDriverState extends State<WaitingForDriver>
       minChildSize: 0.2,
       maxChildSize: 0.9,
       builder: (context, scrollController) {
-        return Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.3 * _childSize,
-              width: double.infinity,
-              padding: EdgeInsets.all(15.sp),
-              decoration: BoxDecoration(
-                color: AppColors.semiGrey.withOpacity(0.8),
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(40.r),
-                  topLeft: Radius.circular(40.r),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                          child: Text(
+        return Container(
+          width: double.infinity,
+          height: _childSize * MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: AppColors.semiGrey.withOpacity(0.8),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(40.r),
+              topLeft: Radius.circular(40.r),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(15.sp),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
                         "Waiting for driver",
                         style: TextStyles.font18WhiteRegular
                             .copyWith(fontWeight: FontWeight.bold),
-                      )),
-                      CircleAvatar()
-                    ],
-                  ),
-                  verticalSpacing(20.h),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: GestureDetector(
-                  onVerticalDragUpdate: (details) {
-                    double newHeight = _childSize -
-                        details.primaryDelta! /
-                            MediaQuery.of(context).size.height;
-                    if (newHeight > 0.2 && newHeight < 0.9) {
-                      setState(() {
-                        _childSize = newHeight;
-                      });
-                    }
-                  },
-                  child: Container(
-                    width: double.infinity,
-                    height: MediaQuery.of(context).size.height *
-                        0.8 *
-                        _childSize, // Adjusted size
-                    padding: EdgeInsets.all(15.sp),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40.r),
-                        topLeft: Radius.circular(40.r),
                       ),
                     ),
-                    child: ListView(
-                      controller: scrollController,
+                    const CircleAvatar(),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(15.sp),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(40.r),
+                      topLeft: Radius.circular(40.r),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                    controller: scrollController, // Attach the scroll controller
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            BlocBuilder<TripCubit, TripState>(
-                              builder: (context, state) {
-                                if (state is GetTripDetailsLoading) {
-                                  return const CircularProgressIndicator(
-                                    color: AppColors.primary,
-                                  );
-                                } else if (state is GetTripDetailsLoaded) {
-                                  return Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Destination: ${state.trip.to}',
-                                        style: TextStyles.font24BlackBold,
-                                      ),
-                                      Text(
-                                        'From: ${state.trip.from}',
-                                        style: TextStyles.font18BlackRegular,
-                                      ),
-                                      Text(
-                                        'Price: ${state.trip.price} ${state.trip.paymentMethod}',
-                                        style: TextStyles.font18BlackRegular,
-                                      ),
-                                    ],
-                                  );
-                                } else if (state is GetTripDetailsError) {
-                                  safePrint(
-                                      'Error loading trips: ${state.message}');
-                                  return Text('Error: ${state.message}');
-                                }
-                                return const SizedBox();
-                              },
-                            ),
-                            CancelButton(),
-                          ],
+                        BlocBuilder<TripCubit, TripState>(
+                          builder: (context, state) {
+                            if (state is GetTripDetailsLoading) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                  color: AppColors.primary,
+                                ),
+                              );
+                            } else if (state is GetTripDetailsLoaded) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Destination: ${state.trip.to}',
+                                    style: TextStyles.font24BlackBold,
+                                  ),
+                                  Text(
+                                    'From: ${state.trip.from}',
+                                    style: TextStyles.font18BlackRegular,
+                                  ),
+                                  Text(
+                                    'Price: ${state.trip.price} ${state.trip.paymentMethod}',
+                                    style: TextStyles.font18BlackRegular,
+                                  ),
+                                ],
+                              );
+                            } else if (state is GetTripDetailsError) {
+                              safePrint('Error loading trips: ${state.message}');
+                              return Text('Error: ${state.message}');
+                            }
+                            return const SizedBox();
+                          },
                         ),
+                        SizedBox(height: 20.h), // Add spacing
+                        CancelButton(),
                       ],
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
